d44af97568b7c6382378ad1fd9f4da0c9e25a937
Krypto Kajun <kryptokajun@protonmail.com>
2021-10-23 23:16:27 -0500

    don't ask

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+import {
+  log,
+  table
+} from 'console'
+
+import { ethers, BigNumber, providers } from 'ethers'
+
+let network = process.env.NETWORK || process.env.DEFAULT_NETWORK || 'aox'
+
+log(`Backend, Baby!!!`)
